e78a3463646eee2b99a3923225b27091d382fc96
jerry_laptop <jiajiewu1988@gmail.com>
2014-01-26 19:24:03 -0800

    first commit

---
+++ Auction_Project/src/auction/Money.java
@@ -0,0 +1,45 @@
+package auction;
+
+/**
+ * Define a Money Object
+ *
+ */
+
+public class Money {
+	
+	private String currencyUnits;
+	private double amount;
+	
+	public Money(double amount) {
+		this.amount = amount;
+	}
+
+	public Money(String unit, double amount) {
+		currencyUnits = unit;
+		this.amount = amount;
+	}
+	
+	/**
+	 * Get the amount of the Money object
+	 * @return amount of Money
+	 */
+	public double getAmount() {
+		return amount;
+	}
+
+	/**
+	 * Set the amount of Money
+	 * @param amount the amount of money gonna be set
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	/**
+	 * Get the amount in Euro
+	 * @return the amount in Euro
+	 */
+	public double getAmountInEuro() {
+		return (double) 10.00 * amount / 7.00;
+	}
+}
